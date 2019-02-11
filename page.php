276e7d1a6abb5b4e13eda8ba684a7a564fa8f566
Alex Neverov <al_neverov@live.ru>
2019-02-11 23:52:05 +0500

    Posts navigation added

--- page.php
+++ page.php
@@ -17,7 +17,7 @@
 		<?php if (have_posts()):  ?>
 			<div class="row m-0">
 				<?php while(have_posts()): the_post(); ?>
-					<div class="col-12 pb-2 border border-info rounded">
+					<div class="col-12 pb-2 pt-2 border border-info rounded">
 						<?php if (has_post_thumbnail()): ?>
 							<img src="<?php the_post_thumbnail_url(); ?>" />
 						<?php endif; ?>
@@ -32,14 +32,13 @@
 						<p><?php the_category(); ?></p>
 						<p><?php the_tags(); ?></p>
 					</div>
+					<div class="posts-navigation col-12 d-flex flex-row justify-content-between pt-2">
+						<?php previous_post_link('%link'); ?>
+						<?php next_post_link('%link'); ?>
+    			</div>
 				<?php endwhile; ?>
 			</div>
 
-			<div class="d-flex flex-row justify-content-center pt-2">
-				<ul class="pagination">
-					<?php _e(paginate_links()); ?>
-				</ul>
-			</div>
 		<?php endif; ?>
 	</div>
 
