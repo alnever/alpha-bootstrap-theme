b4c74cba84c1b9a6899ae45dcff83dae5367a83d
Alex Neverov <al_neverov@live.ru>
2019-02-12 03:16:44 -0800

    Widgets: Calenar, Meta, Navigation. SASS: extend row and col-* classes for responcive layouts

--- archive.php
+++ archive.php
@@ -13,37 +13,48 @@
 	</div>
 
 	<!-- site body -->
-	<div class="container p-0">
-		<?php if (have_posts()):  ?>
-			<div class="row m-0">
-				<?php while(have_posts()): the_post(); ?>
-					<div class="col-4 pb-2">
-						<?php if (has_post_thumbnail()): ?>
-							<img src="<?php the_post_thumbnail_url(); ?>" />
-						<?php endif; ?>
-						<h3><?php the_title(); ?></h3>
-						<div class="">
-								<span><?php the_date(); ?></span>
-								<span><?php the_author_link(); ?></span>
-						</div>
+	<div class="container p-0 mb-2">
+		<div class="row">
+			<!-- posts -->
+			<div class="col-9">
+				<?php if (have_posts()):  ?>
+					<div class="row m-0">
+						<?php while(have_posts()): the_post(); ?>
+							<div class="col-6 pb-2">
+								<?php if (has_post_thumbnail()): ?>
+									<img src="<?php the_post_thumbnail_url(); ?>" />
+								<?php endif; ?>
+								<h3><?php the_title(); ?></h3>
+								<div class="">
+										<span><?php the_date(); ?></span>
+										<span><?php the_author_link(); ?></span>
+								</div>
 
-						<p><?php the_excerpt(); ?></p>
-						<p><?php the_category(); ?></p>
-						<div class="d-flex flex-row justify-content-end">
-							<a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">
-								<?php _e('More...', 'alpha'); ?>
-							</a>
-						</div>
+								<p><?php the_excerpt(); ?></p>
+								<p><?php the_category(); ?></p>
+								<div class="d-flex flex-row justify-content-end">
+									<a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">
+										<?php _e('More...', 'alpha'); ?>
+									</a>
+								</div>
+							</div>
+						<?php endwhile; ?>
 					</div>
-				<?php endwhile; ?>
-			</div>
 
-			<div class="d-flex flex-row justify-content-center pt-2">
-				<ul class="pagination">
-					<?php _e(paginate_links()); ?>
-				</ul>
+					<div class="d-flex flex-row justify-content-center pt-2">
+						<ul class="pagination">
+							<?php _e(paginate_links()); ?>
+						</ul>
+					</div>
+				<?php endif; ?>
 			</div>
-		<?php endif; ?>
+			<!-- sidebar -->
+			<div class="col-3">
+				<?php
+					dynamic_sidebar('right_sidebar');
+				?>
+			</div>
+		</div>
 	</div>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -11,83 +11,101 @@
 			</p>
 		</div>
 	</div>
+
 	<!-- sticky post -->
 	<div class="container p-0">
-		
-			<?php 
-				$sticky = get_option('stiky_posts');
-				$args = [
-					'posts_per_page' => 1,
-					'post__in' => $stiky,
-					'ignore_stiky_posts' => 1,
-				];
-				$query = new WP_Query($args);
-				while ( $query->have_posts() ): $query->the_post();
-			?>
-				<div class="row p-2 m-0 mb-2 border border-info rounded bg-light">
-					<!-- stiky post featured image -->
-					<?php if (has_post_thumbnail()): ?>
-						<div class="col-6 p-2">
-							<img src="<?php the_post_thumbnail_url(); ?>" />
-						</div>
-					<?php endif; ?>
-
-					<!-- stiky post creditials -->
-					<div class="<?php _e(has_post_thumbnail() ? 'col-6' : 'col-12') ?>">
-						<h2><?php the_title() ?></h2>
-						<div class="">
-							<span><?php the_date(); ?></span>
-							<span><?php the_author_link(); ?></span>
-						</div>
-						<p><?php the_excerpt(); ?></p>
-						<p><?php the_category(); ?></p>
-						<div class="d-flex flex-row justify-content-end">
-							<a href="<?php the_permalink(); ?>" class="btn btn-success">
-								<?php _e('More...', 'alpha'); ?>
-							</a>
-						</div>
-					</div>
-				</div>
-			<?php
-				endwhile;
-				wp_reset_postdata();
-			?>
-		
+		<?php
+			dynamic_sidebar('header_widgets');
+		?>
 	</div>
 
 
 	<!-- site body -->
-	<div class="container p-0">
-		<?php if (have_posts()):  ?>
-			<div class="row m-0">
-				<?php while(have_posts()): the_post(); ?>
-					<div class="col-4 pb-2">
+	<div class="container p-0 mb-2">
+		<div class="row">
+			<!-- posts -->
+			<div class="col-9">
+				<!-- sticky post -->
+				<?php
+					$sticky = get_option('stiky_posts');
+					$args = [
+						'posts_per_page' => 1,
+						'post__in' => $stiky,
+						'ignore_stiky_posts' => 1,
+					];
+					$query = new WP_Query($args);
+					while ( $query->have_posts() ): $query->the_post();
+				?>
+					<div class="row p-2 m-0 mb-2 border border-info rounded bg-light">
+						<!-- stiky post featured image -->
 						<?php if (has_post_thumbnail()): ?>
-							<img src="<?php the_post_thumbnail_url(); ?>" />
+							<div class="col-6 p-2">
+								<img src="<?php the_post_thumbnail_url(); ?>" />
+							</div>
 						<?php endif; ?>
-						<h3><?php the_title(); ?></h3>
-						<div class="">
+
+						<!-- stiky post creditials -->
+						<div class="<?php _e(has_post_thumbnail() ? 'col-6' : 'col-12') ?>">
+							<h2><?php the_title() ?></h2>
+							<div class="">
 								<span><?php the_date(); ?></span>
 								<span><?php the_author_link(); ?></span>
-						</div>
-
-						<p><?php the_excerpt(); ?></p>
-						<p><?php the_category(); ?></p>
-						<div class="d-flex flex-row justify-content-end">
-							<a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">
-								<?php _e('More...', 'alpha'); ?>
-							</a>
+							</div>
+							<p><?php the_excerpt(); ?></p>
+							<p><?php the_category(); ?></p>
+							<div class="d-flex flex-row justify-content-end">
+								<a href="<?php the_permalink(); ?>" class="btn btn-success">
+									<?php _e('More...', 'alpha'); ?>
+								</a>
+							</div>
 						</div>
 					</div>
-				<?php endwhile; ?>
-			</div>
+				<?php
+					endwhile;
+					wp_reset_postdata();
+				?>
+				<!-- main post - list -->
+				<?php if (have_posts()):  ?>
+					<div class="row m-0">
+						<?php while(have_posts()): the_post(); ?>
+							<div class="col-6 pb-2">
+								<?php if (has_post_thumbnail()): ?>
+									<img src="<?php the_post_thumbnail_url(); ?>" />
+								<?php endif; ?>
+								<h3><?php the_title(); ?></h3>
+								<div class="">
+										<span><?php the_date(); ?></span>
+										<span><?php the_author_link(); ?></span>
+								</div>
 
-			<div class="d-flex flex-row justify-content-center pt-2">
-				<ul class="pagination">
-					<?php _e(paginate_links()); ?>
-				</ul>
+								<p><?php the_excerpt(); ?></p>
+								<p><?php the_category(); ?></p>
+								<div class="d-flex flex-row justify-content-end">
+									<a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">
+										<?php _e('More...', 'alpha'); ?>
+									</a>
+								</div>
+							</div>
+						<?php endwhile; ?>
+					</div>
+
+					<div class="d-flex flex-row justify-content-center pt-2">
+						<ul class="pagination">
+							<?php _e(paginate_links()); ?>
+						</ul>
+					</div>
+				<?php endif; ?>
 			</div>
-		<?php endif; ?>
+			<!-- end of posts area -->
+
+
+			<!-- sidebar -->
+			<div class="col-3">
+				<?php
+					dynamic_sidebar('right_sidebar');
+				?>
+			</div>
+		</div>
 	</div>
 
 <?php get_footer(); ?>
