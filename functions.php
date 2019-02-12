0a79c472ba23d3060204a0786f5163461f34d5b1
Alex Neverov <al_neverov@live.ru>
2019-02-12 00:15:41 -0800

    Sticky posts enabled

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 	load_theme_textdomain('alpha', get_template_directory().'/languages');
 
 	// allow additional features within the theme
-	add_theme_support('post_thumbnails');
+	add_theme_support('post-thumbnails');
 
 	// register navigation menus
 	register_nav_menus([
--- index.php
+++ index.php
@@ -11,6 +11,50 @@
 			</p>
 		</div>
 	</div>
+	<!-- sticky post -->
+	<div class="container p-0">
+		
+			<?php 
+				$sticky = get_option('stiky_posts');
+				$args = [
+					'posts_per_page' => 1,
+					'post__in' => $stiky,
+					'ignore_stiky_posts' => 1,
+				];
+				$query = new WP_Query($args);
+				while ( $query->have_posts() ): $query->the_post();
+			?>
+				<div class="row p-2 m-0 mb-2 border border-info rounded bg-light">
+					<!-- stiky post featured image -->
+					<?php if (has_post_thumbnail()): ?>
+						<div class="col-6 p-2">
+							<img src="<?php the_post_thumbnail_url(); ?>" />
+						</div>
+					<?php endif; ?>
+
+					<!-- stiky post creditials -->
+					<div class="<?php _e(has_post_thumbnail() ? 'col-6' : 'col-12') ?>">
+						<h2><?php the_title() ?></h2>
+						<div class="">
+							<span><?php the_date(); ?></span>
+							<span><?php the_author_link(); ?></span>
+						</div>
+						<p><?php the_excerpt(); ?></p>
+						<p><?php the_category(); ?></p>
+						<div class="d-flex flex-row justify-content-end">
+							<a href="<?php the_permalink(); ?>" class="btn btn-success">
+								<?php _e('More...', 'alpha'); ?>
+							</a>
+						</div>
+					</div>
+				</div>
+			<?php
+				endwhile;
+				wp_reset_postdata();
+			?>
+		
+	</div>
+
 
 	<!-- site body -->
 	<div class="container p-0">
