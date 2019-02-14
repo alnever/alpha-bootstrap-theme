bf8545c7bfc97839a0d90bdbd0e16f652e46084c
Alex Neverov <al_neverov@live.ru>
2019-02-14 01:31:31 -0800

    Comments: comments tree, form and pagination

--- single.php
+++ single.php
@@ -2,38 +2,34 @@
 
 	<?php if (have_posts()):  ?>
 	<?php while(have_posts()): the_post(); ?>
-		<!-- site header -->
 
 		<!-- site body -->
 		<div class="container p-0 border border-info rounded">
-				<?php if (has_post_thumbnail()): ?>
-					<img src="<?php the_post_thumbnail_url(); ?>" />
-				<?php endif; ?>
 
-
-					<div class="col-12 pb-2 pt-2 ">
-						<div class="header-post">
-							<div class="header-text-block">
-								<div class="post-title">
-									<?php the_title(); ?>
-								</div>
-								<div class="post-info">
-									<span><?php the_date(); ?></span>
-									<span><?php the_author_link(); ?></span>
-								</div>
+				<div class="col-12 pb-2 pt-2 ">
+					<div class="header-post">
+						<div class="header-text-block">
+							<div class="post-title">
+								<?php the_title(); ?>
+							</div>
+							<div class="post-info">
+								<span><?php the_date(); ?></span>
+								<span><?php the_author_link(); ?></span>
 							</div>
 						</div>
-
-						<p><?php the_content(); ?></p>
-		    			<hr>
-						<p><?php the_category(); ?></p>
-		    			<p><?php the_tags(); ?></p>
-					</div>
-		  			<div class="posts-navigation col-12 d-flex flex-row justify-content-between pt-2">
-						<?php previous_post_link('%link'); ?>
-		      			<?php next_post_link('%link'); ?>
 					</div>
 
+					<p><?php the_content(); ?></p>
+	    			<hr>
+					<p><?php the_category(); ?></p>
+	    			<p><?php the_tags(); ?></p>
+				</div>
+	  			<div class="posts-navigation col-12 d-flex flex-row justify-content-between pt-2">
+					<?php previous_post_link('%link'); ?>
+	      			<?php next_post_link('%link'); ?>
+				</div>
+
+				<?php comments_template(); ?>
 
 		</div>
 	<?php endwhile; ?>
